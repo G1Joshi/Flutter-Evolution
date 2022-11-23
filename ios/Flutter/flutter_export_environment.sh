c0264d9a417b60480cd20fb284b76e615c50065f
Jeevan Joshi <joshijeevan98@gmail.com>
2022-11-23 20:20:13 +0530

    v1.12.13

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\G1Joshi\fvm\versions\v1.9.1+hotfix.6"
+export "FLUTTER_ROOT=C:\Users\G1Joshi\fvm\versions\v1.12.13+hotfix.9"
 export "FLUTTER_APPLICATION_PATH=D:\Coding\Project\Personal\flutter_evolution"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\G1Joshi\fvm\versions\v1.9.1+hotfix.6\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\G1Joshi\fvm\versions\v1.12.13+hotfix.9\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
