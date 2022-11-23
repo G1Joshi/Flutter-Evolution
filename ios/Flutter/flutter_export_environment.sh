021605d8e2a48bab1928e61a21cf431ce0bffc0e
Jeevan Joshi <joshijeevan98@gmail.com>
2022-11-23 19:36:32 +0530

    v1.9.1

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Source\flutter"
+export "FLUTTER_ROOT=C:\Users\G1Joshi\fvm\versions\v1.9.1+hotfix.6"
 export "FLUTTER_APPLICATION_PATH=D:\Coding\Project\Personal\flutter_evolution"
-export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\G1Joshi\fvm\versions\v1.9.1+hotfix.6\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=true"
-export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
