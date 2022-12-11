211b332eeaf6f27b4653bf3a3774fa48644d691f
Jeevan Joshi <joshijeevan98@gmail.com>
2022-12-11 14:58:54 +0530

    v2.5.3

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\G1Joshi\fvm\versions\2.5.2"
+export "FLUTTER_ROOT=C:\Users\G1Joshi\fvm\versions\2.5.3"
 export "FLUTTER_APPLICATION_PATH=D:\Coding\Project\Personal\flutter_evolution"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
